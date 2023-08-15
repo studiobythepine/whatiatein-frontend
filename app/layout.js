edf2bd9c38b79dd3ab0bd8e2aa27257dfe66a374
Studio by the pine <105303869+studiobythepine@users.noreply.github.com>
2023-08-15 16:17:28 +0300

    css

---
+++ app/layout.js
@@ -0,0 +1,16 @@
+export default function RootLayout({
+    // Layouts must accept a children prop.
+    // This will be populated with nested layouts or pages
+    children,
+  }) {
+    return (
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    )
+  }
+
+export const metadata = {
+    title: 'Home',
+    description: 'Welcome to Next.js',
+    }
--- pages/[slug].js
+++ pages/[slug].js
@@ -6,26 +6,31 @@ const Place = ({ review }) => {
   const router = useRouter();
 
   return (
-    <div className="absolute top-28 flex flex-col w-full text-cyan-900  md:w-3/4 md:top-0 md:left-1/4 md:h-full">
-      <div className="w-auto h-1/2 relative">
-        <Image
-          src={review[0].attributes.image.data[0].attributes.formats.medium.url}
-          alt={review[0].attributes.name}
-          fill
-          style={{ objectFit: "scale-down" }}
-        ></Image>
-      </div>
-      <div className="flex flex-col md:w-1/2 self-center">
-        <div className="top flex justify-center">
-          <h2 className="text-4xl">{review[0].attributes.name}</h2>
-          <h3>
-            {review[0].attributes.place.data.attributes.city}, {review[0].attributes.place.data.attributes.country}
-          </h3>
+    <div className="absolute top-28 flex flex-col w-full text-cyan-900 md:w-3/4 md:top-0 md:left-1/4 md:h-full">
+      <div className="flex w-full h-1/2">
+        <div className="relative w-3/4">
+          <Image
+            src={review[0].attributes.image.data[0].attributes.formats.large.url}
+            alt={review[0].attributes.name}
+            layout="fill"
+            className="absolute object-center object-cover"
+            // fill
+            // style={{ objectFit: "scale-down" }}
+            // width={review[0].attributes.image.data[0].attributes.formats.medium.width}
+            // height={review[0].attributes.image.data[0].attributes.formats.medium.height}
+            // style={{ minWidth:" 100%", minHeight:"100%", objectFit: "cover"}}
+          ></Image>
         </div>
-        <div className="bottom text-lg">
+        <div className="flex flex-col self-center text-center w-1/4">
+            <h2 className="text-4xl">{review[0].attributes.name}</h2>
+            <h3>
+              {review[0].attributes.place.data.attributes.city}, {review[0].attributes.place.data.attributes.country}
+            </h3>
+        </div>
+      </div>
+        <div className="bottom text-lg md:mx-72 md:my-10 self-center ">
           <p>{review[0].attributes.content}</p>
         </div>
-      </div>
     </div>
   );
 };
